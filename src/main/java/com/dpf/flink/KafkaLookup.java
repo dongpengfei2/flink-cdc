a62b71bb22b3888a16855ef8e8ad8a93be93370a
dongpengfei1 <dongpengfei1@shihuituan.com>
2021-06-26 16:15:46 +0800

    binlog to json

--- src/main/java/com/dpf/flink/KafkaLookup.java
+++ src/main/java/com/dpf/flink/KafkaLookup.java
@@ -3,7 +3,10 @@ package com.dpf.flink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
-public class KafkaTranslate {
+/**
+ * 该方法的运行需要修改源码，添加format的源数据
+ */
+public class KafkaODS {
     public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(env);
